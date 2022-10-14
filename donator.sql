d7c7823653224be3e386bead6c0dd7dadf3ba5d7
Devin Monro <mage1d@yahoo.com>
2022-10-13 22:17:55 -0400

    Update donator.sql

--- donator.sql
+++ donator.sql
@@ -1,14 +1,14 @@
 DROP TABLE IF EXISTS `donator`;
 CREATE TABLE `donator` (
-	`license` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
-	`coins` INT(11) NOT NULL DEFAULT 0,
-	UNIQUE INDEX `license` (`license`) USING BTREE
-)
+    `license` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+    `coins` INT(11) NOT NULL DEFAULT 0,
+    UNIQUE INDEX `license` (`license`) USING BTREE
+);
 
 DROP TABLE IF EXISTS `donator_pending`;
 CREATE TABLE `donator_pending` (
-	`transactionId` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
-	`package` LONGTEXT NOT NULL COLLATE 'utf8_general_ci',
-	`redeemed` INT(11) NOT NULL DEFAULT '0',
-	PRIMARY KEY (`transactionId`) USING BTREE
-)
+    `transactionId` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
+    `package` LONGTEXT NOT NULL COLLATE 'utf8_general_ci',
+    `redeemed` INT(11) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`transactionId`) USING BTREE
+);
